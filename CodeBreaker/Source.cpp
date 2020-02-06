de1fb3dfb7aba709ef7330d622c115974f62b1ab
Jackinthebox <jacob_dominski@yahoo.com>
2020-02-06 14:17:43 -0700

    3 games

--- CodeBreaker/Source.cpp
+++ CodeBreaker/Source.cpp
@@ -7,60 +7,70 @@ using namespace std;
 
 int main()
 {
-	enum fields {WORD, HINT, NUM_FIELDS};
-	const int NUM_WORDS = 5;
-	const string WORDS[NUM_WORDS][NUM_FIELDS] =
-	{
-		{"wall", "Do you feel you're banging your head against something?"},
-		{"glasses", "These might help you see the answer."},
-		{"labored", "Going slowly, is it?"},
-		{"persistent", "Keep at it."},
-		{"jumble", "It's what the game is all about."}
-	};
-	srand(static_cast<unsigned int>(time(0)));
-	int choice = (rand() % NUM_WORDS);
-	string theWord = WORDS[choice][WORD];
-	string theHint = WORDS[choice][HINT];
-
-	string jumble = theWord;
-	int length = jumble.size();
-	for(int i = 0; i < length; i++)
-	{
-		int index1 = rand() % length;
-		int index2 = rand() % length;
-		char temp = jumble[index1];
-		jumble[index1] = jumble[index2];
-		jumble[index2] = temp;
-	}
-
 	cout << "\t\t\tWelcome to Word Jumble!\n\n";
 	cout << "Unscramble the letters to make a word.\n";
-	cout << "Enter 'hint' for a hint.\n";
-	cout << "Enter 'quit' tpo quit the game.\n";
-	cout << "The jumble is: " << jumble;
 
-	string guess;
-	cout << "\n\nYour guess:>";
-	cin >> guess;
-	
-	while ((guess != theWord) && (guess != "quit"))
+	for(int x = 0;x < 3;x++)
 	{
-		if (guess == "hint")
+		enum fields {WORD, HINT, NUM_FIELDS};
+		const int NUM_WORDS = 5;
+		const string WORDS[NUM_WORDS][NUM_FIELDS] =
 		{
-			cout << theHint;
-		}
-		else
+			{"wall", "Do you feel you're banging your head against something?"},
+			{"glasses", "These might help you see the answer."},
+			{"labored", "Going slowly, is it?"},
+			{"persistent", "Keep at it."},
+			{"jumble", "It's what the game is all about."}
+		};
+		srand(static_cast<unsigned int>(time(0)));
+		int choice = (rand() % NUM_WORDS);
+		string theWord = WORDS[choice][WORD];
+		string theHint = WORDS[choice][HINT];
+
+		string jumble = theWord;
+		int length = jumble.size();
+		for(int i = 0; i < length; i++)
 		{
-			cout << "Sorry, that's not it.";
+			int index1 = rand() % length;
+			int index2 = rand() % length;
+			char temp = jumble[index1];
+			jumble[index1] = jumble[index2];
+			jumble[index2] = temp;
 		}
+
+		
+		cout << "Enter 'hint' for a hint.\n";
+		cout << "Enter 'quit' tpo quit the game.\n";
+		cout << "The jumble is: " << jumble;
+
+		string guess;
 		cout << "\n\nYour guess:>";
 		cin >> guess;
 		
+		while ((guess != theWord) && (guess != "quit"))
+		{
+			if (guess == "hint")
+			{
+				cout << theHint;
+			}
+			else
+			{
+				cout << "Sorry, that's not it.";
+			}
+			cout << "\n\nYour guess:>";
+			cin >> guess;
+			
+		}
+		if (guess == theWord)
+		{
+			cout << "\nThat's it! You guess it!\n";
+		}
+		cout << "\nThanks for playing.\n";
+		if(guess == "quit")
+		{
+			break;
+		}
+		
 	}
-	if (guess == theWord)
-	{
-		cout << "\nThat's it! You guess it!\n";
-	}
-	cout << "\nThanks for playing.\n";
 	return 0;
 }
